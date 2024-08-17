09d90a9c9493581022754de576f57ee5b4ee56b9
fawad404 <fawadanxari31@gmail.com>
2024-08-17 10:23:35 -0700

    test-page

--- app/page.js
+++ app/page.js
@@ -6,19 +6,18 @@ import Contactfourth from "./components/contactfourth/Contactfourth";
 import Impressumfifth from "./components/impressumfifth/Impressumfifth";
 import Datenschutz from "./components/datenschutz/Datenschutz";
 import UserAppointment from "./components/userAppointment/UserAppointment";
-import TestFirst from "./components/testFirst/TestFirst";
+
 
 const Home = () => {
   return (
     <>
-    <TestFirst />
-    {/* <UserAppointment /> */}
-    {/* <Herosecond />
+    <Herosecond />
     <Herothird />
     <Informationthird />
     <Contactfourth />
     <Impressumfifth />
-    <Datenschutz /> */}
+    <Datenschutz />
+    <UserAppointment />
     </>
   );
 };
---
+++ app/test/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TestFirst from '../components/testFirst/TestFirst';
+
+const Page = () => {
+  return (
+    <TestFirst />
+  )
+}
+
+export default Page;
